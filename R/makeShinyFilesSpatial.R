05bfa8961afec8cf4f59839cc42e71481424b2c1
Ryan Routsong <routsongrm@nih.gov>
2025-12-08 11:56:24 -0500

    https://github.com/the-ouyang-lab/ShinyCell2/pull/20

--- R/makeShinyFilesSpatial.R
+++ R/makeShinyFilesSpatial.R
@@ -30,16 +30,34 @@ makeShinyFilesSpatial <- function(
   sc1image = list()
   if(class(obj)[1] == "Seurat"){
     if(.hasSlot(obj, "images")){
-      # coordinates
-      sc1image$coord <- obj@images[[1]]@coordinates
+      # coordinates - handle both VisiumV1 and VisiumV2 formats
+      visium_image <- obj@images[[1]]
+      if ("VisiumV2" %in% class(visium_image)) {
+        # New VisiumV2 format - coordinates are in boundaries$centroids
+        coords <- visium_image@boundaries$centroids@coords
+        # Convert to expected format with imagecol and imagerow columns
+        sc1image$coord <- data.frame(
+          tissue = 1,  # Default tissue value
+          row = NA,    # Not available in VisiumV2
+          col = NA,    # Not available in VisiumV2  
+          imagecol = coords[, "x"],
+          imagerow = coords[, "y"]
+        )
+        rownames(sc1image$coord) <- rownames(coords)
+      } else {
+        # Traditional VisiumV1 format
+        sc1image$coord <- visium_image@coordinates
+      }
+      
       # uncropped background image
       bg_image <- GetImage(obj, mode = "raster") # background image
       # bg_grob <- rasterGrob(bg_image, width=unit(1,"npc"), height=unit(1,"npc"), 
       #                       interpolate = FALSE) # background image grob
       sc1image$bg_image <- bg_image
       # sc1image$bg_grob  <- bg_grob
+      
       # for cropped image generation in the server.R script
-      sc1image$lowres   <- obj@images[[1]]@scale.factors$lowres
+      sc1image$lowres   <- visium_image@scale.factors$lowres
 
     }
   } else {
