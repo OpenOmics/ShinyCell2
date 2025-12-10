76922b028c37715bdf83672ca238b2375d094cc9
Ryan Routsong <routsongrm@nih.gov>
2025-12-10 10:23:22 -0500

    fix: check cell id presence in assay before serializing

--- R/makeH5fromSeurat.R
+++ R/makeH5fromSeurat.R
@@ -21,15 +21,23 @@ makeH5fromSeurat <- function(obj, sc1meta, filename,
   # Create h5 file and get ready
   if(class(obj@assays[[gex.assay]]) == "Assay5"){
     gex.matdim = dim(obj@assays[[gex.assay]]@layers[[gex.slot]])
+    # Get available cells in this assay
+    assay.cells = colnames(obj@assays[[gex.assay]]@layers[[gex.slot]])
   }else{
     gex.matdim = dim(slot(obj@assays[[gex.assay]], gex.slot))
+    # Get available cells in this assay
+    assay.cells = colnames(slot(obj@assays[[gex.assay]], gex.slot))
   }
+  
+  # Filter sc1meta to only include cells present in this assay
+  sc1meta.filtered = sc1meta[sc1meta$cellID %in% assay.cells, ]
+  
   sc1gexpr <- H5File$new(filename, mode = "w")
   sc1gexpr.grp <- sc1gexpr$create_group("grp")
   sc1gexpr.grp.data <- sc1gexpr.grp$create_dataset(
     "data",  dtype = h5types$H5T_NATIVE_FLOAT,
-    space = H5S$new("simple", dims = gex.matdim, maxdims = gex.matdim),
-    chunk_dims = c(1,gex.matdim[2]))
+    space = H5S$new("simple", dims = c(gex.matdim[1], nrow(sc1meta.filtered)), maxdims = c(gex.matdim[1], nrow(sc1meta.filtered))),
+    chunk_dims = c(1, nrow(sc1meta.filtered)))
   chk = chunkSize
   while(chk > (gex.matdim[1]-8)){
     chk = floor(chk / 2)     # Account for cases where nGene < chunkSize
@@ -42,22 +50,22 @@ makeH5fromSeurat <- function(obj, sc1meta, filename,
     for(i in 1:nChunk){
       sc1gexpr.grp.data[((i-1)*chk+1):(i*chk), ] <- as.matrix(
         obj[[gex.assay]][gex.slot][
-          ((i-1)*chk+1):(i*chk), sc1meta$cellID])
+          ((i-1)*chk+1):(i*chk), sc1meta.filtered$cellID])
     }
       sc1gexpr.grp.data[(i*chk+1):gex.matdim[1], ] <- as.matrix(
         obj[[gex.assay]][gex.slot][
-          (i*chk+1):gex.matdim[1], sc1meta$cellID])
+          (i*chk+1):gex.matdim[1], sc1meta.filtered$cellID])
       gex.rownm = rownames(obj[[gex.assay]][gex.slot])
       
   } else {
     for(i in 1:nChunk){
       sc1gexpr.grp.data[((i-1)*chk+1):(i*chk), ] <- as.matrix(
         slot(obj@assays[[gex.assay]], gex.slot)[
-          ((i-1)*chk+1):(i*chk), sc1meta$cellID])
+          ((i-1)*chk+1):(i*chk), sc1meta.filtered$cellID])
     }
       sc1gexpr.grp.data[(i*chk+1):gex.matdim[1], ] <- as.matrix(
         slot(obj@assays[[gex.assay]], gex.slot)[
-          (i*chk+1):gex.matdim[1], sc1meta$cellID])
+          (i*chk+1):gex.matdim[1], sc1meta.filtered$cellID])
       gex.rownm = rownames(slot(obj@assays[[gex.assay]], gex.slot))
   }
   sc1gexpr$close_all()
